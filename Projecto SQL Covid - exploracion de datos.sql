a5e913ff9ba1500bcda2000707171281acfa4d1f
IgnacioBrunotto <133723726+IgnacioBrunotto@users.noreply.github.com>
2023-08-28 13:33:36 -0300

    Projecto SQL Covid - exploracion de datos.sql

--- Projecto SQL Covid - exploracion de datos.sql
+++ Projecto SQL Covid - exploracion de datos.sql
@@ -23,18 +23,21 @@ order by 1,2
 
 --Looking at total cases vs population
 --Muestra el porcentage de la poblacion infectada en determinado momento.
+	
 Select location, date, total_cases, population, (total_cases/population) *100 as 'Infected percentage of the population'
 from ProjectCovid..CovidDeaths$
 where location like '%argentina%'
 order by 1,2
 
 --Looking for countries with highest infection rate compared with population
+	
 Select location,  population ,MAX(total_cases) AS highest_infection_count , MAX((total_cases/population)) *100 as PercentPopulationInfected
 from ProjectCovid..CovidDeaths$
 Group by population, location
 order by PercentPopulationInfected desc
 
 --Looking for countries with highest death count by population
+	
 Select location,MAX(cast(total_deaths as int)) AS highest_death_count
 from ProjectCovid..CovidDeaths$
 Where continent is not null
@@ -42,6 +45,7 @@ Group by location
 order by highest_death_count desc
 
 --Break down by continents
+	
 Select location ,MAX(cast(total_deaths as int)) AS highest_death_count
 from ProjectCovid..CovidDeaths$
 Where continent is null 
@@ -49,6 +53,7 @@ Group by location
 order by highest_death_count desc
 
 --global numbers
+	
 Select  SUM(new_cases) as TotalCases, SUM(cast(new_deaths as int)) as TotalDeaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as DeathPercetange 
 from ProjectCovid..CovidDeaths$
 Where continent is not null
@@ -57,6 +62,7 @@ order by 1,2
 
 --Join de las tablas
 --Poblacion total vacunaciones
+	
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 sum(convert(int, vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as rolling_ball_vac
 from ProjectCovid..CovidDeaths$ dea
